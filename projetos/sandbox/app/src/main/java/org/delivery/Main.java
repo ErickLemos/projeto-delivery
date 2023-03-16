82bce78e90f5fa1269aff09fd70bfe1fa48a72e8
Erick B. Lemos <ericklemoscosta@gmail.com>
2023-03-16 19:41:40 -0300

    ajustes

--- projetos/sandbox/app/src/main/java/org/delivery/Main.java
+++ projetos/sandbox/app/src/main/java/org/delivery/Main.java
@@ -5,20 +5,25 @@ public class Main {
     // Programa deve receber um nivel de fraquancia e então retornar/imprimir a flor que está naquele nível
     public static void main(String[] args) {
 
-        var nivelDeFraquancia = 1;
+        Integer nivelDeFraquancia = 1;
 
-        var margaridaNome = "margarida";
-        var margaridaNivel = 1;
+        // margarida
+        String margaridaNome = "margarida";
+        Integer margaridaNivel = 1;
 
-        var rosaNome = "rosa";
-        var rosaNivel = 2;
+        // rosa
+        String rosaNome = "rosa";
+        Integer rosaNivel = 2;
 
-        var florSuperCheirosaNome = "flor super cheirosa";
-        var florSuperCheirosaNivel = 3;
+        // flor super cheirosa
+        String florSuperCheirosaNome = "flor super cheirosa";
+        Integer florSuperCheirosaNivel = 3;
 
-        var resultadoNome = "";
-        var resultadoNivel = 0;
+        // resultado
+        String resultadoNome = "";
+        Integer resultadoNivel = 0;
 
+        // resultado - logic
         if (nivelDeFraquancia == 1) {
             resultadoNome = margaridaNome;
             resultadoNivel = margaridaNivel;
