1400a34c903e108f861264dad8290f581948d318
Erick B. Lemos <ericklemoscosta@gmail.com>
2023-03-24 14:59:47 -0300

    segurança

--- projetos/carteira/app/src/main/java/com/delivery/carteira/CarteiraApplication.java
+++ projetos/carteira/app/src/main/java/com/delivery/carteira/CarteiraApplication.java
@@ -2,7 +2,6 @@ package com.delivery.carteira;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 // TODO: implementar validacoes de entradas
 
