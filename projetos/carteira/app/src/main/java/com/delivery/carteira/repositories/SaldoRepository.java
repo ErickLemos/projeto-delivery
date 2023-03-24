39ab47519ed084b2aec76a7940d48daf70306876
Erick B. Lemos <ericklemoscosta@gmail.com>
2023-03-24 13:07:21 -0300

    segurança

--- projetos/carteira/app/src/main/java/com/delivery/carteira/repositories/SaldoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.delivery.carteira.repositories;
-
-import com.delivery.carteira.repositories.entities.SaldoEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SaldoRepository extends MongoRepository<SaldoEntity, String> {
-}
